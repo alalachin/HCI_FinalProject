4d8df9e73ad73111ff4dbb9e6ef83d3f589aa183
alalachin <105703050@nccu.edu.tw>
2020-09-27 01:42:01 +0800

    Add files via upload

---
+++ TimeNum.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeNum : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            float intime = KinectManager.nowTime;
+            if (intime > 20.0f)
+            {
+                //Debug.Log("finish");
+                GameObject.Find("finish").GetComponent<UnityEngine.UI.Text>().text = "恭喜!完成時間為" + intime.ToString("F1");
+            }
+        }
+    }
+}
---
+++ time.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI; //使用Unity UI程式庫。
+
+public class time : MonoBehaviour
+{
+
+    int time_int = 5;
+
+    public Text time_UI;
+
+    void Start()
+    {
+
+        InvokeRepeating("timer", 1, 1);
+
+    }
+
+    void timer()
+    {
+
+        time_int -= 1;
+
+        time_UI.text = time_int-1 + "";
+
+        if (time_int == 1)
+        {
+
+            time_UI.text = "Go";
+
+        }
+        if(time_int == 0)
+        {
+            time_UI.text = " ";
+            CancelInvoke("timer");
+        }
+
+    }
+
+}
